89c550f4bc53f13f6bce437399aad5c2fdca8e7a
bence <bencekollar23@gmail.com>
2018-02-20 20:07:53 +0100

    updated version(sys clear,tie func)

--- valami2.py
+++ valami2.py
@@ -1,10 +1,9 @@
+import os
 A = [" ", " ", " ", " ", " ", " ", " ", " ", " ", " "]
 
-
 def board():
-    """
-    Draw the board.
-    """
+    os.system('clear')
+    
     print()
     print('   |   |   ')
     print('', A[7], '|', A[8], '|', A[9], '')
@@ -18,62 +17,80 @@ def board():
     print('', A[1], '|', A[2], '|', A[3], '')
     print('   |   |   ')
     print()
-tie=0
 
 board()
-
+opoints=0
+xpoints=0
+tie=0
 while True:
     while True:
         if tie==9:
-            print("Döntetlen")
+            print("It's a tie!")
         location = int(input("choose a location for O: "))
         tie+=1
         if A[location] == " ":
             A[location] = "O"
             break
         elif A[location] != " ":
-            print("foglalt")
+            print("Occupied")
     board()
+    
+    
 
     for i in range(1, 8, 3):  # (Ez vizsgálja a vízszintes sorokat, hogy megvan-e a három "O".)
         if A[i] == "O" and A[i+1] == "O" and A[i+2] == "O":
-            print("O a nyertes")
+            opoints+=1
+            print("O is the winner!")
+            print(Opoints)
 
     for i in range(1, 4):  # (Ez vizsgálja a függőleges sorokat, hogy megvan-e a három "O".)7
         if A[i] == "O" and A[i+3] == "O" and A[i+6] == "O":
-            print("O a nyertes")
+            opoints+=1
+            print("O is the winner!")
+            print(Opoints)
 
     if (A[1]== "O" and A[5]== "O" and A[9]== "O"):
-            print("O won the game!")
+            opoints+=1
+            print("O is the winner!")
+            print(Opoints)
 
     if (A[3]== "O" and A[5]== "O" and A[7]== "O"):
-            print("O won the game!")
+            opoints+=1
+            print("O is the winner!")
+            print(Opoints)
 #------------------------------------------------------------------------------#
     while True:
         if tie==9:
-            print("Döntetlen")
+            print("It's a tie!")
         location = int(input("choose a location for X: "))
         tie+=1
         if A[location] == " ":
             A[location] = "X"
             break
         elif A[location] != " ":
-            print("foglalt")
+            print("Occupied")
     board()
+    
+
     for i in range(1, 8, 3):  # (Ez vizsgálja a vízszintes sorokat, hogy megvan-e a három "X".)
         if A[i] == "X" and A[i+1] == "X" and A[i+2] == "X":
-            print("X a nyertes")
+            xpoints+=1
+            print("X is the winner!")
+            print(Xpoints)
 
     for i in range(1, 4):  # (Ez vizsgálja a vízszintes sorokat, hogy megvan-e a három "X".)
         if A[i] == "X" and A[i+3] == "X" and A[i+6] == "X":
-            print("X a nyertes")
-
+            xpoints+=1
+            print("X is the winner!")
+            print(Xpoints)
     if (A[1]== "X" and A[5]== "X" and A[9]== "X"):
-            print("X won the game!")
-
+            xpoints+=1
+            print("X is the winner!")
+            print(Xpoints)
     if (A[3]== "X" and A[5]== "X" and A[7]== "X"):
-            print("X won the game!")
-            
+            xpoints+=1
+            print("X is the winner!")
+            print(Xpoints)
 
 
 
