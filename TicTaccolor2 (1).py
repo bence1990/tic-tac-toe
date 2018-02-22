0790ba020b2c2365a8ffea760cebf50b7d3a863b
bence <bencekollar23@gmail.com>
2018-02-22 13:01:42 +0100

    I added:def restart_game(), def main(), managed to apply a restart function so if you get a tie or win-- it will ask if you want to restart.

--- TicTaccolor2 (1).py
+++ TicTaccolor2 (1).py
@@ -1,10 +1,9 @@
 import os
-A = [" ", " ", " ", " ", " ", " ", " ", " ", " ", " "]
+import sys
 
-
-def board():
+def board(A):
     os.system('clear')
-
+    
     print()
     print('   |   |   ')
     print('', A[7], '|', A[8], '|', A[9], '')
@@ -18,71 +17,95 @@ def board():
     print('', A[1], '|', A[2], '|', A[3], '')
     print('   |   |   ')
     print()
-tie=0
 
-board()
-#-------------------------------------------------------------
-"""def restart_game(restart):
-    answercontinue= input("whould you like to restart?(y/n): ")
+
+
+def restart_game():
+    restart= input("whould you like to restart?(y/n): ")
     if restart=="y":
-        restart=0
-    else:
-        restart=1
-        print("Good bye")
-        sys.exit"""
-#-------------------------------------------------------------
+        main()
+    if restart=="n":
+        sys.exit()
+        
 
-O = '\033[1;32mO\033[1;m'
-X = '\033[1;34mX\033[1;m'
+def main():
+    A = [" ", " ", " ", " ", " ", " ", " ", " ", " ", " "]
+    board(A) 
+    tie=0
+    O = '\033[1;32mO\033[1;m'
+    X = '\033[1;34mX\033[1;m'
+    end = False
+    while not end:
+        while True:
+            if tie==9:
+                print("It's a Tie!")
+                end = True
+                break
+            location = int(input("Choose a location for O: "))
+            tie+=1
+            if A[location] == " ":
+                A[location] = O
+                break
+            elif A[location] != " ":
+                print("Occupied space!")
+        board(A)
 
-while True:
-    while True:
-        if tie==9:
-            print("It's a Tie!")
-        location = int(input("Choose a location for O: "))
-        tie+=1
-        if A[location] == " ":
-            A[location] = O
-            break
-        elif A[location] != " ":
-            print("Occupied space!")
-    board()
+        for i in range(1, 8, 3):  # (Ez vizsgálja a vízszintes sorokat, hogy megvan-e a három "O".)
+            if A[i] == O and A[i+1] == O and A[i+2] == O:
+                end = True
+                print("O won the game!")
+                break
 
-    for i in range(1, 8, 3):  # (Ez vizsgálja a vízszintes sorokat, hogy megvan-e a három "O".)
-        if A[i] == O and A[i+1] == O and A[i+2] == O:
-            print("O won the game!")
+        for i in range(1, 4):  # (Ez vizsgálja a függőleges sorokat, hogy megvan-e a három "O".)7
+            if A[i] == O and A[i+3] == O and A[i+6] == O:
+                end = True
+                print("O won the game!")
+                break
 
-    for i in range(1, 4):  # (Ez vizsgálja a függőleges sorokat, hogy megvan-e a három "O".)7
-        if A[i] == O and A[i+3] == O and A[i+6] == O:
-            print("O won the game!")
+        if (A[1]== O and A[5]== O and A[9]== O):
+                end = True
+                print("O won the game!")
+                break
 
-    if (A[1]== O and A[5]== O and A[9]== O):
-            print("O won the game!")
+        if (A[3]== O and A[5]== O and A[7]== O):
+                end = True
+                print("O won the game!")
+                break
+    #------------------------------------------------------------------------------#
+        while True:
+            if tie==9:
+                print("It's a Tie!")
+                end = True
+                break
+            location = int(input("Choose a location for X: "))
+            tie+=1
+            if A[location] == " ":
+                A[location] = X
+                break
+            elif A[location] != " ":
+                print("Occupied space!")
+        board(A)
+        for i in range(1, 8, 3):  # (Ez vizsgálja a vízszintes sorokat, hogy megvan-e a három "X".)
+            if A[i] == X and A[i+1] == X and A[i+2] == X:
+                end = True
+                print("X won the game!")
+                break
 
-    if (A[3]== O and A[5]== O and A[7]== O):
-            print("O won the game!")
-#------------------------------------------------------------------------------#
-    while True:
-        if tie==9:
-            print("It's a Tie!")
-        location = int(input("Choose a location for X: "))
-        tie+=1
-        if A[location] == " ":
-            A[location] = X
-            break
-        elif A[location] != " ":
-            print("Occupied space!")
-    board()
-    for i in range(1, 8, 3):  # (Ez vizsgálja a vízszintes sorokat, hogy megvan-e a három "X".)
-        if A[i] == X and A[i+1] == X and A[i+2] == X:
-            print("X won the game!")
+        for i in range(1, 4):  # (Ez vizsgálja a vízszintes sorokat, hogy megvan-e a három "X".)
+            if A[i] == X and A[i+3] == X and A[i+6] == X:
+                end = True
+                print("X won the game!")
+                break
 
-    for i in range(1, 4):  # (Ez vizsgálja a vízszintes sorokat, hogy megvan-e a három "X".)
-        if A[i] == X and A[i+3] == X and A[i+6] == X:
-            print("X won the game!")
+        if (A[1]== X and A[5]== X and A[9]== X):
+                end = True
+                print("X won the game!")
+                break
 
-    if (A[1]== X and A[5]== X and A[9]== X):
-            print("X won the game!")
-
-    if (A[3]== X and A[5]== X and A[7]== X):
-            print("X won the game!")
+        if (A[3]== X and A[5]== X and A[7]== X):
+                end = True
+                print("X won the game!")
+                break
+    if end:
+        restart_game()
+main()      
